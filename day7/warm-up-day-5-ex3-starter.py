fdbdf0d2ae10278a486a2b14a1ed924ce48c656c
Brandon Tardio <btardio@gmail.com>
2018-12-08 10:36:59 -0800

    Update warm-up-day-5-ex3-starter.py

--- day7/warm-up-day-5-ex3-starter.py
+++ day7/warm-up-day-5-ex3-starter.py
@@ -1,1 +1,59 @@
 
+
+# checks that the matrix is rectangular
+def checkjagged(lst):
+    
+    size = len(lst[0])
+    
+    for inner in lst:
+        if len(inner) != size: raise Exception('Jagged List')
+
+# returns the dimensions of the matrix
+def dim(a):
+    
+    height = len(a)
+    width = len(a[0])
+    
+    return (height, width)
+
+# adds two matrices
+def matrixadd(lsta, lstb):
+    
+    checkjagged(lsta)
+    checkjagged(lstb)
+    
+    if ( dim(lsta) != dim(lstb) ): raise Exception('Dimensions not the same.')
+    
+    out = []
+    
+    for (rowa, rowb) in zip(lsta, lstb):
+        
+        newlst = []
+        
+        for (itma, itmb) in zip(rowa,rowb):
+            
+            newlst.append( int(itma) + int(itmb) )
+            
+        out.append(newlst)
+        
+    return out
+
+
+def convertFileToList(filename):
+
+    rvalmatrix = []
+
+    # TODO
+            
+    return rvalmatrix
+    
+
+matA = convertFileToList('matrixA.txt')
+matB = convertFileToList('matrixB.txt')
+
+assert(matrixadd(matA, matB) == [[6,8],[10,12]])
+
+matC = convertFileToList('matrixC.txt')
+matD = convertFileToList('matrixD.txt')
+
+assert(matrixadd(matC, matD) == [[1,1,1],[1,1,1],[1,1,1]])
